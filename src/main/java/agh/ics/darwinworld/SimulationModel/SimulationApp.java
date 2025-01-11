c36123fc66243cdd087ad485caddcd6e9aa7545e
Marcin Szulc <mszulc@student.agh.edu.pl>
2025-01-11 15:56:56 +0100

    work on simulation, work on application model

---
+++ src/main/java/agh/ics/darwinworld/SimulationModel/SimulationApp.java
@@ -0,0 +1,14 @@
+package agh.ics.darwinworld.SimulationModel;
+
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class SimulationApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+    private void configureStage(Stage primaryStage, BorderPane viewRoot) {
+    }
+}
---
+++ src/main/java/agh/ics/darwinworld/WorldGUI.java
@@ -0,0 +1,10 @@
+package agh.ics.darwinworld;
+
+import agh.ics.darwinworld.SimulationModel.SimulationApp;
+import javafx.application.Application;
+
+public class WorldGUI {
+    public static void main(String[] args) {
+        Application.launch(SimulationApp.class, args);
+    }
+}
