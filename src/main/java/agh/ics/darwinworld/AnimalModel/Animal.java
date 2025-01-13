72c27b528725bcdc7147a8aca4bdd24a760d6533
Iwo <iwozowada4@gmail.com>
2025-01-13 14:59:25 +0100

    Mostly changes in Animal

--- src/main/java/agh/ics/darwinworld/AnimalModel/Animal.java
+++ src/main/java/agh/ics/darwinworld/AnimalModel/Animal.java
@@ -1,17 +1,29 @@
 package agh.ics.darwinworld.AnimalModel;
 
 import agh.ics.darwinworld.Enums.MapDirection;
+import agh.ics.darwinworld.SimulationModel.Simulation;
 import agh.ics.darwinworld.Util.Vector2d;
 import agh.ics.darwinworld.WorldModel.WorldMap;
+
+import java.util.HashSet;
+import java.util.Random;
 
 public class Animal {
     private MapDirection direction;
     private Vector2d position;
     private String genome;
-    private int energy_level;
+    private int energyLevel;
 
-    public Animal(Vector2d initialPosition) {
+    public Animal(Vector2d initialPosition, String genome, int basicEnergyLevel) {
+        Random rand = new Random();
         this.position = initialPosition;
+        this.genome = genome;
+        this.energyLevel = basicEnergyLevel;
+        this.direction = MapDirection.NORTH;
+        int random = rand.nextInt(8);
+        for(int i = 0; i < random; i++){
+            this.direction = direction.next();
+        }
     }
 
     public Vector2d getPosition() {
@@ -20,6 +32,14 @@ public class Animal {
 
     public MapDirection getOrientation() {
         return this.direction;
+    }
+
+    public String getGenome(){
+        return this.genome;
+    }
+
+    public int getEnergyLevel(){
+        return this.energyLevel;
     }
 
     @Override
@@ -32,6 +52,7 @@ public class Animal {
     }
 
     public void move(String movement, WorldMap map){
+        //movement jest juz charem ktory zawiera informacje o ilosci obrotu w danym dniu
         Vector2d newposition;
 
         for(int i = 0; i < Integer.valueOf(movement); i++){
@@ -44,4 +65,84 @@ public class Animal {
             this.position = newposition;
         }
     }
+
+    //consume
+
+
+    //podajemy animale ktore wczesniej sprawdzamy ze sa zdolne do rozmnazania i stoja na tym samym polu
+    public void reproduce(Animal parent1, Animal parent2){
+        Random rand = new Random();
+
+        //tworzenie genomu młodego
+        String youngGenome;
+        boolean side = rand.nextBoolean();
+        int firstEnergy = parent1.getEnergyLevel();
+        int secondEnergy = parent2.getEnergyLevel();
+        String firstGenome = parent1.getGenome();
+        String secondGenome = parent2.getGenome();
+        int n = firstGenome.length();
+        int partition = (firstEnergy / (firstEnergy + secondEnergy)) * n;
+
+        if(side){
+            youngGenome = firstGenome.substring(0, partition) + secondGenome.substring(partition, n);
+        }
+        else{
+            youngGenome = secondGenome.substring(0,partition) + firstGenome.substring(partition, n);
+        }
+
+        //mutacje
+        //trzeba dorobic zeby wersja 2 sie robila tylko wtedy gdy uzytkownik tak wybierze w GUI
+
+        //wersja normalna
+        HashSet mutatedPlaces = new HashSet<>();
+        int mutationCount = rand.nextInt(n+1);
+        int newGene;
+        String oldGene;
+
+        int i = 0;
+        while(i < mutationCount){
+            int mutationPlace = rand.nextInt(n);
+            if (!mutatedPlaces.contains(mutationPlace)){
+                mutatedPlaces.add(mutationPlace);
+                do {
+                    newGene = rand.nextInt(8);
+                    oldGene = youngGenome.substring(newGene, newGene+1);
+                } while(Integer.valueOf(oldGene) == newGene);
+
+                char[] youngGenomeChars = youngGenome.toCharArray();
+                youngGenomeChars[mutationPlace] = (char) newGene;
+                youngGenome = String.valueOf(youngGenomeChars);
+                i++;
+            }
+        }
+
+        //wersja 2
+
+        boolean change = rand.nextBoolean();
+        int firstChangePlace;
+        int secondChangePlace;
+
+        if (change){
+            firstChangePlace = rand.nextInt(n);
+            do{
+                secondChangePlace = rand.nextInt(n);
+            }while(firstChangePlace == secondChangePlace);
+
+            char[] youngGenomeChars = youngGenome.toCharArray();
+            char buf = youngGenomeChars[firstChangePlace];
+            youngGenomeChars[firstChangePlace] = youngGenomeChars[secondChangePlace];
+            youngGenomeChars[secondChangePlace] = buf;
+            youngGenome = String.valueOf(youngGenomeChars);
+        }
+
+        // tworzenie energii młodego
+        // przegadac czy dajemy stala wartosc czy procent energii
+        int energyLevel = 10;
+
+        Animal addedAnimal = new Animal(position, youngGenome, energyLevel);
+        Simulation.animals.add(addedAnimal);
+        Simulation.worldMap.place(addedAnimal);
+
+    }
+
 }
--- src/main/java/agh/ics/darwinworld/SimulationModel/Simulation.java
+++ src/main/java/agh/ics/darwinworld/SimulationModel/Simulation.java
@@ -7,11 +7,31 @@ import java.util.List;
 
 public class Simulation {
     private List<Vector2d> positions;
-    private List<Animal> animals;
-    private WorldMap worldMap;
+    public static List<Animal> animals;
+    public static WorldMap worldMap;
+    private List<String> genomes;
+    private int startEnergyLevel;
 
-    public Simulation(List<Vector2d> positions, WorldMap worldMap) {
+
+    public Simulation(List<Vector2d> positions, WorldMap worldMap, List<String> genomes, int startEnergyLevel) {
         this.positions = positions;
         this.worldMap = worldMap;
+<<<<<<< Updated upstream
+=======
+        this.animals = new ArrayList<Animal>();
+        int i = 0;
+        for (Vector2d position : positions) {
+            Animal addedAnimal = new Animal(position, genomes.get(i), startEnergyLevel);
+            animals.add(addedAnimal);
+            worldMap.place(addedAnimal);
+            i++;
+        }
+    }
+
+    public List<Animal> getAnimals() {return animals;}
+
+    public void run(){
+        //tutaj animale będą się ruszać po kolei
+>>>>>>> Stashed changes
     }
 }
