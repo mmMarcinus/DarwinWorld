3413a1cc67de57b280ba71363b1a4fe3c408e2a1
Marcin Szulc <mszulc@student.agh.edu.pl>
2025-01-16 18:47:21 +0100

    Revert "work on simultionvieW"
    This reverts commit 0840d1e21144a90d134c88e609c1e1415e1983d0.

--- src/main/java/agh/ics/darwinworld/ApplicationModel/SimulationPresenter.java
+++ src/main/java/agh/ics/darwinworld/ApplicationModel/SimulationPresenter.java
@@ -1,13 +1,10 @@
 package agh.ics.darwinworld.ApplicationModel;
 
-import agh.ics.darwinworld.Records.WorldParameters;
-import agh.ics.darwinworld.View.AnimalView;
-import agh.ics.darwinworld.View.EmptyTileView;
-import agh.ics.darwinworld.View.PlantView;
 import agh.ics.darwinworld.WorldModel.Abstracts.MapChangeListener;
-import agh.ics.darwinworld.WorldModel.Abstracts.WorldMap;
 import agh.ics.darwinworld.WorldModel.NormalWorldMap;
+import agh.ics.darwinworld.WorldModel.Abstracts.WorldMap;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -15,18 +12,10 @@ import javafx.scene.layout.RowConstraints;
 
 public class SimulationPresenter implements MapChangeListener {
 
-    private WorldParameters worldParameters;
-
     private NormalWorldMap normalWorldMap;
 
     @FXML
     private GridPane mapGrid;
-    @FXML
-    private GridPane worldGrid;
-
-    public void setWorldParameters(WorldParameters worldParameters){
-        this.worldParameters = worldParameters;
-    }
 
     public void setWorldMap(NormalWorldMap normalWorldMap) {
         this.normalWorldMap = normalWorldMap;
@@ -41,37 +30,26 @@ public class SimulationPresenter implements MapChangeListener {
 
 
     public void drawMap(){
-        mapGrid.getChildren().clear();
-        mapGrid.add(new AnimalView(),0,0);
-        mapGrid.add(new AnimalView(),1,0);
-        mapGrid.add(new AnimalView(),0,1);
-        mapGrid.add(new PlantView(), 3, 3);
-        mapGrid.add(new PlantView(), 1, 1);
-        mapGrid.add(new AnimalView(),3,0);
-        mapGrid.add(new EmptyTileView(),2,1);
-        mapGrid.add(new EmptyTileView(),2,2);
+        //tutaj po kolei rysuje wszytkie rzeczy na mapie
+        mapGrid.getChildren().clear(); // Usuwanie starych elementów z siatki
 
-        //tutaj bede roibl
-//        //tutaj po kolei rysuje wszytkie rzeczy na mapie
-//        mapGrid.getChildren().clear(); // Usuwanie starych elementów z siatki
-//
-//
-//        for (int i = 0; i < normalWorldMap.getWidth(); i++) {
-//            mapGrid.getColumnConstraints().add(new ColumnConstraints(10)); // szerokość komórki
-//        }
-//        for (int j = 0; j < normalWorldMap.getHeight(); j++) {
-//            mapGrid.getRowConstraints().add(new RowConstraints(10)); // wysokość komórki
-//        }
-//
-//        for (int i = 0; i < normalWorldMap.getWidth(); i++) {
-//            for (int j = 0; j < normalWorldMap.getHeight(); j++) {
-//                Label cell = new Label(); // Pusta komórka
-//                cell.setStyle("-fx-border-color: black; -fx-background-color: white;"); // Styl komórek
-//                cell.setPrefSize(10, 10); // Rozmiar komórki
-//                mapGrid.add(cell, i, j); // Dodanie komórki na odpowiednie miejsce w siatce
-//            }
-//        }
-//        mapGrid.setGridLinesVisible(true);
+
+        for (int i = 0; i < normalWorldMap.getWidth(); i++) {
+            mapGrid.getColumnConstraints().add(new ColumnConstraints(10)); // szerokość komórki
+        }
+        for (int j = 0; j < normalWorldMap.getHeight(); j++) {
+            mapGrid.getRowConstraints().add(new RowConstraints(10)); // wysokość komórki
+        }
+
+        for (int i = 0; i < normalWorldMap.getWidth(); i++) {
+            for (int j = 0; j < normalWorldMap.getHeight(); j++) {
+                Label cell = new Label(); // Pusta komórka
+                cell.setStyle("-fx-border-color: black; -fx-background-color: white;"); // Styl komórek
+                cell.setPrefSize(10, 10); // Rozmiar komórki
+                mapGrid.add(cell, i, j); // Dodanie komórki na odpowiednie miejsce w siatce
+            }
+        }
+        mapGrid.setGridLinesVisible(true);
     }
 
     @Override
--- src/main/java/agh/ics/darwinworld/SimulationModel/SimulationApp.java
+++ src/main/java/agh/ics/darwinworld/SimulationModel/SimulationApp.java
@@ -4,9 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class SimulationApp extends Application {
