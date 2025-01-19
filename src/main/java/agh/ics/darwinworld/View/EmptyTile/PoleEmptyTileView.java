c88e72975eb64a2f705049c48f48e33d78485f9f
Iwo <iwozowada4@gmail.com>
2025-01-19 18:44:07 +0100

    Poles nearly done

---
+++ src/main/java/agh/ics/darwinworld/View/EmptyTile/PoleEmptyTileView.java
@@ -0,0 +1,7 @@
+package agh.ics.darwinworld.View.EmptyTile;
+
+public class PoleEmptyTileView extends EmptyTileView {
+    public PoleEmptyTileView() {
+        super.setStyle("-fx-background-color: #95bb65; -fx-background-image: url('/images/Pole.png'); -fx-background-size: 100% 100%; -fx-background-color: transparent;");
+    }
+}
